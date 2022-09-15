641ed1826f25eb6f741ffd44f2e4c0966e7e0723
Robert Barney <heshan.sandeepa@freshmilksoftware.com>
2022-09-15 13:57:58 +0530

    proceed

---
+++ AndroidArchitecturePatterns/app/src/main/java/com/heshan/androidarchitecturepatterns/pure_mvc/NoteController.kt
@@ -0,0 +1,9 @@
+package com.heshan.androidarchitecturepatterns.pure_mvc
+
+import com.heshan.androidarchitecturepatterns.mvc.NoteRepository
+import com.heshan.androidarchitecturepatterns.mvc.NotesFragment
+
+class NoteController(val notesFragment: NotesFragment, val noteRepository: NoteRepository) {
+
+
+}
